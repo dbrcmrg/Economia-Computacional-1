6ed2114e599d579a629cd7a9510bd2ef650bcf47
dbrcmrg <104875645+dbrcmrg@users.noreply.github.com>
2024-08-22 18:20:43 -0300

    Add files via upload

---
+++ imprime_casa/imprime_casa.py
@@ -0,0 +1,22 @@
+"""
+Programa "imprime_casa"
+Descrição: Este programa imprime a palavra casa na tela
+Autor: Débora Camargo
+Data: 06/08/2024
+Versão: 0.0.1
+"""
+
+# Alocação de memória
+
+
+# Entrada de dados
+
+
+# Processamento de dados
+
+
+# Saída de dados
+
+# Este código imprime a palavra casa na tela
+
+print("casa")
